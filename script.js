0ec19f822066ae6cd3e66e9974f1dab96092f8fd
Roger Medellin <medr33@yahoo.com>
2023-03-10 00:57:32 -0500

    working on selectors for passwords

--- script.js
+++ script.js
@@ -5,30 +5,59 @@ var number = [1,2,3,4,5,6,7,8,9,0]
 var symbol = ["!","@","#","$","%","^","&","*","(",")","_",
 "-","=","+"
 ,"`","~","<",">","?","/",",",".",";",":","'" ]
-var 
+var toUppercase
+var toLowercase
 
 
 // take in user information
 function userInfo(){
-var length = 
-console.log(length)
+
+  const userAnswer = prompt("Click OK to confirm including special characters.");
+
+  const userAnswer = prompt("Click OK to confirm numeric characters.");
+  
+  const userAnswer = prompt("Click OK to confirm including uppercase characters.");
+  
+  const userAnswer = prompt("Click OK to confirm including lowercash characters.");
+
+
+
+
+//var length = 
+//cosnole.log(length)
 }
 
 
 //process input info
 function generatePassword(){
+
 var userAnswers = userInfo();
 
-let person = prompt("How many characters would you like you password to contain?")
+
+// length of the password
+const passwordLength = prompt("How many characters would you like your password to be?");
+
+let passwordLength = parseInt(prompt("How many characters would you like your password to be?"));
 
 
-if (userAnswer"Click OK to confirm including special characters") {
-  
-("Click OK to confirm including lowercase characters.")}
-("Click OK to confirm including upercase characters.")
-console.log(userAnswer)
+// validate input
+while(isNaN(passwordLength) || passwordLength <= 0) {
+  passwordLength = parseInt(prompt("Please enter a valid number for password length."));
+}
+// Password requirements
 
+const userAnswer = prompt("Click OK to confirm including special characters.");
 
+const userAnswer = prompt("Click OK to confirm numeric characters.");
+
+const userAnswer = prompt("Click OK to confirm including uppercase characters.");
+
+const userAnswer = prompt("Click OK to confirm including lowercash characters.");
+
+// if (userAnswer"Click OK to confirm including special characters") {
+  // ("Click OK to confirm including lowercase characters.")}
+// ("Click OK to confirm including upercase characters.")
+// console.log(userAnswer)
 }
 // const pass_el = document.querySelector("password span")
 // const length =16
@@ -46,8 +75,7 @@ console.log(userAnswer)
 
   //  expression1 = (x >= 8);
   // var expression2 = (x <=)128;
-     
-    //return 12;
+
 
 // }
 
